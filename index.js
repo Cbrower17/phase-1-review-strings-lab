0d3e4afecb1491cd507d9d8ef653c2a4697fd32b
Christopher <chris.brower.96@gmail.com>
2023-01-30 14:54:54 -0700

    working lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+let currentUser = 'Chris';
+let welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+let excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser.toUpperCase() + '!';
+let shortGreeting = 'Welcome, ' + currentUser.substring(0,1) + '!';
